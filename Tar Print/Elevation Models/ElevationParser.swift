e02967198967cf71306a034612d330d0ae2c7c8d
Suraj Vaddi <surajnvaddi@gmail.com>
2021-08-21 11:12:42 -0400

    Initial commit

---
+++ Tar Print/Elevation Models/ElevationParser.swift
@@ -0,0 +1,8 @@
+//
+//  ElevationParser.swift
+//  Tar Print
+//
+//  Created by Suraj Vaddi on 12/21/20.
+//
+
+import UIKit
---
+++ Tar Print/Image Models/ElevationImageContainer.swift
@@ -0,0 +1,28 @@
+//
+//  ElevationImageContainer.swift
+//  Tar Print
+//
+//  Created by Suraj Vaddi on 12/23/20.
+//
+
+import UIKit
+import MapKit
+
+struct ElevationImageContainer{
+    
+    /*
+    CONTAINS ALL ELEVATION INFORMATION
+     */
+    
+    public static var image: UIImage!
+    public static var rgbaImage: RGBAImage!
+    
+    public static var latDelta: Float!
+    public static var longDelta: Float!
+    
+    public static var latTop: Float!
+    public static var longRight: Float!
+    public static var latBottom: Float!
+    public static var longLeft: Float!
+    
+}
